06a26bc095d04845b428c4510f2b6ed280e5b030
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2017-06-28 15:45:25 -0300

    forcefully select master

--- init.rb
+++ init.rb
@@ -17,3 +17,4 @@ Autobuild::CMake.show_make_messages = true
 Autoproj.config.separate_prefixes = true
 Autobuild::CMake.delete_obsolete_files_in_prefix = Autoproj.config.separate_prefixes?
 
+Autoproj.config.set 'ROCK_SELECTED_FLAVOR', 'master'
